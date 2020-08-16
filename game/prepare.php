a8a65b51989835e31bc21a1cf25b3bd0a7ccd4e7
孙英洲 <sunyingzhou@sunyingzhoudeMacBook-Pro.local>
2020-08-16 15:32:49 +0800

    game1

--- game/prepare.php
+++ game/prepare.php
@@ -1,3 +1,13 @@
 <?php
-//接收棋盘信息
+    //接收棋盘信息
+    session_start();
+    include("include/util.inc.php");
+    checkLegal();
+
+    $userid = $_POST['userid'];
+    $chessBoard = $_POST['chessboard'];
+
+    $userid = $PDO->quote($userid);
+    $chessBoard = $PDO->quote($chessBoard);
+    $PDO->exec("INSERT INTO $CHESSBOARD_TABLE (userid,chessboard) VALUES($userid, $chessBoard)");
 ?>
--- game/waitstart.php
+++ game/waitstart.php
@@ -1,4 +1,15 @@
 <?php
 //等待开始，发送指令告诉两端开始游戏
+    session_start();
+    include("include/util.inc.php");
+    checkLegal();
 
+    $userid = $_GET['userid'];
+    $userid = $PDO->quote($userid);
+
+    $row = $PDO->query("SELECT * FROM $BATTLE_TABLE WHERE user1=$userid OR user2=$userid")->fetch();
+
+    if($row['']){
+        echo json_encode([]);
+    }
 ?>
