188dc4e8e5ce1588ba445548c3547ea8d1bf8f60
unknown <lstkpeter@gmail.com>
2020-08-18 21:04:52 +0800

    .

--- reset.php
+++ reset.php
@@ -5,10 +5,14 @@
 
     $userid = $_SESSION['userid'];
     $useridforsql = $PDO->quote($userid);
-
+    $opponent = $_COOKIE['opponent'];
+    $opponentforsql = $PDO->quote($opponent);
+    //chessboard删除两条
     $PDO->exec("DELETE FROM $CHESSBOARD_TABLE WHERE userid=$useridforsql");
-
+    $PDO->exec("DELETE FROM $CHESSBOARD_TABLE WHERE userid=$opponentforsql");
+    //games删除一条
     $PDO->exec("DELETE FROM $GAME_TABLE WHERE user1=$useridforsql OR user2=$useridforsql");
-
+    //steps删除两条
     $PDO->exec("DELETE FROM $STEP_TABLE WHERE userid=$useridforsql");
+    $PDO->exec("DELETE FROM $STEP_TABLE WHERE userid=$opponentforsql");
 ?>
